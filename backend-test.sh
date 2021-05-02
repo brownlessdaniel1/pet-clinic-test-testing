9b9ce068e95915afce997ccae6c49a960a77bb4b
brownlessdaniel1 <DBrownless@qa.com>
2021-05-02 20:29:11 +0000

    db: trying envar fix

--- backend-test.sh
+++ backend-test.sh
@@ -1,6 +1,7 @@
 sudo apt-get update -y
 sudo apt-get install maven -y
 sudo apt install openjdk-8-jre
+JAVA_HOME=$"/usr/lib/jvm/java-8-openjdk-amd64/jre"
 mvn --version
 cd spring-petclinic-rest-master
 mvn test
